3e3f41620d33073724d9b14a1f01218b8e870b94
meleks-61 <maibalonn@gmail.com>
2022-02-25 20:59:42 +0300

    useReducer is created

--- expense_tracker/src/components/AddTransaction.js
+++ expense_tracker/src/components/AddTransaction.js
@@ -1,22 +1,25 @@
-import React from "react";
+import React,{useState} from "react";
 
 
 
 
 export const AddTransaction=()=>{
+    const [text,setText]=useState('')
+    const [amount,setAmount]=useState(0)
     return(
         <>
         <h3>Add new transaction</h3>
         <form >
             <div className="form-control">
-                <label for="text">Text</label>
-                <input for="text" placeholder="enter text..."/>
+                <label htmlFor="text">Text</label>
+                <input htmlFor="text" value={text} onChange={(e)=>setText(e.target.value)} placeholder="enter text..."/>
             </div>
 
             <div className="form-control">
-                <label for="amount">Amount <br/>  (negative-expense,positive-income)</label>
-                <input for="number"  placeholder="enter amount..."/>
+                <label htmlFor="amount">Amount <br/>  (negative-expense,positive-income)</label>
+                <input htmlFor="number" value={amount} onChange={(e)=>setAmount(e.target.value)} placeholder="enter amount..."/>
             </div>
+            <button className="btn">Add transaction</button>
 
 
         </form>
--- expense_tracker/src/components/TransactionList.js
+++ expense_tracker/src/components/TransactionList.js
@@ -5,7 +5,7 @@ export const TransactionList=()=>{
         <>
 
         <h3>History</h3>
-        <ul id="list" className="list">
+        <ul  className="list">
             <li className="minus">
                 Cash <span>-$400</span><button className="delete-btn">x</button>
             </li>
