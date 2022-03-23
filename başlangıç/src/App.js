670d8ec7b16cebc8e72e376ea0faccae1d24e021
meleks-61 <maibalonn@gmail.com>
2022-03-23 21:47:46 +0300

    structure created

--- başlangıç/src/App.js
+++ başlangıç/src/App.js
@@ -1,1 +1,13 @@
+import React from 'react'
+import './App.scss'
 
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Welcome React</h1>
+    p
+    </div>
+  )
+}
+
+export default App
